fd74438fd081e4b3049cd079038e53d1b1c3207c
Leo Peng <d0214949@gmail.com>
2024-02-05 22:16:40 +0800

    有基本篩選功能的

--- read.py
+++ read.py
@@ -5,8 +5,15 @@ with open('reviews.txt','r') as f:
 		data.append(line)
 		if len(data) % 1000 == 0:
 			print(len(data))
+
 sum_len = 0
 for d in data:
 	sum_len += len(d)
 print(sum_len)
 print(sum_len / len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
